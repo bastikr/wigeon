8aeebb54903950334906ae079eb8bc7816ee5284
Sebastian Kraemer <basti.kr@gmail.com>
2017-10-26 10:39:01 +0200

    Change indentation for c++ files.

--- src/rotations/rotations.cc
+++ src/rotations/rotations.cc
@@ -16,27 +16,27 @@ namespace rotations {
 namespace rotationmatrix {
 
 RotationMatrix x(double alpha) {
-    Matrix3d R;
-    R << 1, 0, 0,
-         0, cos(alpha), -sin(alpha),
-         0, sin(alpha), cos(alpha);
-    return R;
+  Matrix3d R;
+  R << 1, 0, 0,
+       0, cos(alpha), -sin(alpha),
+       0, sin(alpha), cos(alpha);
+  return R;
 }
 
 RotationMatrix y(double alpha) {
-    Matrix3d R;
-    R << cos(alpha), 0, sin(alpha),
-         0, 1, 0,
-         -sin(alpha), 0, cos(alpha);
-    return R;
+  Matrix3d R;
+  R << cos(alpha), 0, sin(alpha),
+       0, 1, 0,
+       -sin(alpha), 0, cos(alpha);
+  return R;
 }
 
 RotationMatrix z(double alpha) {
-    Matrix3d R;
-    R << cos(alpha), -sin(alpha), 0,
-         sin(alpha), cos(alpha), 0,
-         0, 0, 1;
-    return R;
+  Matrix3d R;
+  R << cos(alpha), -sin(alpha), 0,
+       sin(alpha), cos(alpha), 0,
+       0, 0, 1;
+  return R;
 }
 
 } // namespace rotationmatrix
@@ -46,40 +46,40 @@ namespace eulerangles {
 // ZYX Euler angles
 
 EulerAngles x(double alpha) {
-    return Vector3d(0, 0, alpha);
+  return Vector3d(0, 0, alpha);
 }
 
 EulerAngles y(double alpha) {
-    return Vector3d(0, alpha, 0);
+  return Vector3d(0, alpha, 0);
 }
 
 EulerAngles z(double alpha) {
-    return Vector3d(alpha, 0, 0);
+  return Vector3d(alpha, 0, 0);
 }
 
 } // namespace eulerangles
 
 
 EulerAngles rotationmatrix2eulerangles(const RotationMatrix& R) {
-    double beta = atan2(-R(2,0), sqrt(R(0,0)*R(0,0) + R(1,0)*R(1,0)));
-    double c = cos(beta);
-    double alpha = atan2(R(1,0)/c, R(0,0)/c);
-    double gamma = atan2(R(2,1)/c, R(2,2)/c);
-    return Vector3d(alpha, beta, gamma);
+  double beta = atan2(-R(2,0), sqrt(R(0,0)*R(0,0) + R(1,0)*R(1,0)));
+  double c = cos(beta);
+  double alpha = atan2(R(1,0)/c, R(0,0)/c);
+  double gamma = atan2(R(2,1)/c, R(2,2)/c);
+  return Vector3d(alpha, beta, gamma);
 }
 
 RotationMatrix eulerangles2rotationmatrix(const EulerAngles& angles) {
-    double c1 = cos(angles(0));
-    double c2 = cos(angles(1));
-    double c3 = cos(angles(2));
-    double s1 = sin(angles(0));
-    double s2 = sin(angles(1));
-    double s3 = sin(angles(2));
-    Matrix3d R;
-    R << c1*c2, c1*s2*s3 - s1*c3, c1*s2*c3 + s1*s3,
-         s1*c2, s1*s2*s3 + c1*c3, s1*s2*c3 - c1*s3,
-         -s2, c2*s3, c2*c3;
-    return R;
+  double c1 = cos(angles(0));
+  double c2 = cos(angles(1));
+  double c3 = cos(angles(2));
+  double s1 = sin(angles(0));
+  double s2 = sin(angles(1));
+  double s3 = sin(angles(2));
+  Matrix3d R;
+  R << c1*c2, c1*s2*s3 - s1*c3, c1*s2*c3 + s1*s3,
+       s1*c2, s1*s2*s3 + c1*c3, s1*s2*c3 - c1*s3,
+       -s2, c2*s3, c2*c3;
+  return R;
 }
 
 } // namespace rotations
