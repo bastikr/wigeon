56e55c397993178dc634d803ffc260738938c5ff
Sebastian Kraemer <basti.kr@gmail.com>
2017-10-26 14:16:52 +0200

    Conversions between different angle types and tests.

--- src/rotations/rotations.cpp
+++ src/rotations/rotations.cpp
@@ -11,6 +11,45 @@ namespace geometry {
 
 namespace rotations {
 
+RotationMatrix::RotationMatrix(const EulerAngles& E) {
+  double c1 = cos(E.angles(0));
+  double c2 = cos(E.angles(1));
+  double c3 = cos(E.angles(2));
+  double s1 = sin(E.angles(0));
+  double s2 = sin(E.angles(1));
+  double s3 = sin(E.angles(2));
+  data << c1*c2, c1*s2*s3 - s1*c3, c1*s2*c3 + s1*s3,
+          s1*c2, s1*s2*s3 + c1*c3, s1*s2*c3 - c1*s3,
+          -s2, c2*s3, c2*c3;
+}
+
+RotationMatrix::RotationMatrix(const AxisAngle& A) {
+  double c = cos(A.angle);
+  double s = sin(A.angle);
+  data(0,0) = (1-c)*A.axis(0)*A.axis(0) + c;
+  data(0,1) = (1-c)*A.axis(0)*A.axis(1) - s*A.axis(2);
+  data(0,2) = (1-c)*A.axis(0)*A.axis(2) + s*A.axis(1);
+  data(1,0) = (1-c)*A.axis(1)*A.axis(0) + s*A.axis(2);
+  data(1,1) = (1-c)*A.axis(1)*A.axis(1) + c;
+  data(1,2) = (1-c)*A.axis(1)*A.axis(2) - s*A.axis(0);
+  data(2,0) = (1-c)*A.axis(2)*A.axis(0) - s*A.axis(1);
+  data(2,1) = (1-c)*A.axis(2)*A.axis(1) + s*A.axis(0);
+  data(2,2) = (1-c)*A.axis(2)*A.axis(2) + c;
+}
+
+RotationMatrix::RotationMatrix(const Quaternion& Q) {
+  const Vector4d& q = Q.data;
+  data(0,0) = 1 - 2*q(1)*q(1) - 2*q(2)*q(2);
+  data(0,1) = 2*(q(0)*q(1) - q(2)*q(3));
+  data(0,2) = 2*(q(0)*q(2) + q(1)*q(3));
+  data(1,0) = 2*(q(0)*q(1) + q(2)*q(3));
+  data(1,1) = 1 - 2*q(0)*q(0) - 2*q(2)*q(2);
+  data(1,2) = 2*(q(1)*q(2) - q(0)*q(3));
+  data(2,0) = 2*(q(0)*q(2) - q(1)*q(3));
+  data(2,1) = 2*(q(0)*q(3) + q(1)*q(2));
+  data(2,2) = 1 - 2*q(0)*q(0) - 2*q(1)*q(1);
+}
+
 RotationMatrix RotationMatrix::rotate_x(double angle) {
   Matrix3d R;
   R << 1, 0, 0,
@@ -36,6 +75,21 @@ RotationMatrix RotationMatrix::rotate_z(double angle) {
 }
 
 
+EulerAngles::EulerAngles(const RotationMatrix& R) {
+  const Matrix3d& r = R.data;
+  angles(1) = atan2(-r(2,0), sqrt(r(0,0)*r(0,0) + r(1,0)*r(1,0)));
+  double c = cos(angles(1));
+  angles(0) = atan2(r(1,0)/c, r(0,0)/c);
+  angles(2) = atan2(r(2,1)/c, r(2,2)/c);
+}
+
+EulerAngles::EulerAngles(const Quaternion& Q) {
+  const Vector4d& q = Q.data;
+  angles(2) = atan2(2*(q(3)*q(0) + q(1)*q(2)), 1 - 2*(q(0)*q(0) + q(1)*q(1)));
+  angles(1) = asin(2*(q(3)*q(1) - q(2)*q(0)));
+  angles(0) = atan2(2*(q(3)*q(2) + q(0)*q(1)), 1 - 2*(q(1)*q(1) + q(2)*q(2)));
+}
+
 EulerAngles EulerAngles::rotate_x(double angle) {
   return EulerAngles(Vector3d(0, 0, angle));
 }
@@ -49,28 +103,83 @@ EulerAngles EulerAngles::rotate_z(double angle) {
 }
 
 
+AxisAngle::AxisAngle(const RotationMatrix& R) {
+  const Matrix3d& r = R.data;
+  angle = acos((r(0,0) + r(1,1) + r(2,2) - 1)/2);
+  double s = 2*sin(angle);
+  axis(0) = (r(2,1)-r(1,2))/s;
+  axis(1) = (r(0,2)-r(2,0))/s;
+  axis(2) = (r(1,0)-r(0,1))/s;
+}
 
-// EulerAngles rotationmatrix2eulerangles(const RotationMatrix& R) {
-//   double beta = atan2(-R(2,0), sqrt(R(0,0)*R(0,0) + R(1,0)*R(1,0)));
-//   double c = cos(beta);
-//   double alpha = atan2(R(1,0)/c, R(0,0)/c);
-//   double gamma = atan2(R(2,1)/c, R(2,2)/c);
-//   return Vector3d(alpha, beta, gamma);
-// }
+AxisAngle::AxisAngle(const Quaternion& Q) {
+  double n = sqrt(Q.data(0)*Q.data(0) + Q.data(1)*Q.data(1) + Q.data(2)*Q.data(2));
+  axis(0) = Q.data(0)/n;
+  axis(1) = Q.data(1)/n;
+  axis(2) = Q.data(2)/n;
+  angle = 2*acos(Q.data(3));
+}
 
-// RotationMatrix eulerangles2rotationmatrix(const EulerAngles& angles) {
-//   double c1 = cos(angles(0));
-//   double c2 = cos(angles(1));
-//   double c3 = cos(angles(2));
-//   double s1 = sin(angles(0));
-//   double s2 = sin(angles(1));
-//   double s3 = sin(angles(2));
-//   Matrix3d R;
-//   R << c1*c2, c1*s2*s3 - s1*c3, c1*s2*c3 + s1*s3,
-//        s1*c2, s1*s2*s3 + c1*c3, s1*s2*c3 - c1*s3,
-//        -s2, c2*s3, c2*c3;
-//   return R;
-// }
+AxisAngle AxisAngle::rotate_x(double angle) {
+  return AxisAngle(Vector3d(1, 0, 0), angle);
+}
+
+AxisAngle AxisAngle::rotate_y(double angle) {
+  return AxisAngle(Vector3d(0, 1, 0), angle);
+}
+
+AxisAngle AxisAngle::rotate_z(double angle) {
+  return AxisAngle(Vector3d(0, 0, 1), angle);
+}
+
+
+Quaternion::Quaternion(const RotationMatrix& R) {
+  const Matrix3d& r = R.data;
+  data(0) = sqrt(1 + r(0,0) - r(1,1) - r(2,2))/2;
+  data(1) = (r(0,1) + r(1,0))/(4*data(0));
+  data(2) = (r(0,2) + r(2,0))/(4*data(0));
+  data(3) = (r(2,1) + r(1,2))/(4*data(0));
+}
+
+Quaternion::Quaternion(const EulerAngles& E) {
+  double s0 = sin(E.angles(0)/2);
+  double s1 = sin(E.angles(1)/2);
+  double s2 = sin(E.angles(2)/2);
+  double c0 = cos(E.angles(0)/2);
+  double c1 = cos(E.angles(1)/2);
+  double c2 = cos(E.angles(2)/2);
+  data(0) = c0*c1*s2 - s0*s1*c2;
+  data(1) = c0*s1*c2 + s0*c1*s2;
+  data(2) = s0*c1*c2 - c0*s1*s2;
+  data(3) = c0*c1*c2 + s0*s1*s2;
+}
+
+Quaternion::Quaternion(const AxisAngle& A) {
+  double s = sin(A.angle/2);
+  data(0) = A.axis(0)*s;
+  data(1) = A.axis(1)*s;
+  data(2) = A.axis(2)*s;
+  data(3) = cos(A.angle/2);
+}
+
+Quaternion Quaternion::rotate_x(double angle) {
+  double s = sin(angle/2);
+  double c = cos(angle/2);
+  return Quaternion(Vector4d(s, 0, 0, c));
+}
+
+Quaternion Quaternion::rotate_y(double angle) {
+  double s = sin(angle/2);
+  double c = cos(angle/2);
+  return Quaternion(Vector4d(0, s, 0, c));
+}
+
+Quaternion Quaternion::rotate_z(double angle) {
+  double s = sin(angle/2);
+  double c = cos(angle/2);
+  return Quaternion(Vector4d(0, 0, s, c));
+}
+
 
 } // namespace rotations
 
--- src/tests/test_rotations.cpp
+++ src/tests/test_rotations.cpp
@@ -1,11 +1,80 @@
 #include "geometry/rotations.h"
 #include "gtest/gtest.h"
+#include <iostream>
+
 
 using namespace geometry::rotations;
 
+bool approx_equal(const RotationMatrix& R1, const RotationMatrix& R2) {
+  return R1.data.isApprox(R2.data);
+}
+
+bool approx_equal(const EulerAngles& E1, const EulerAngles& E2) {
+  return E1.angles.isApprox(E2.angles);
+}
+
+bool approx_equal(const AxisAngle& A1, const AxisAngle& A2) {
+  return A1.axis.isApprox(A2.axis) && (abs(A1.angle - A2.angle) < 1e-15);
+}
+
+TEST(Rotations, RotationMatrix) {
+  double alpha = 0.12;
+  double beta = 0.23;
+  double gamma = 0.71;
+  auto Rx = RotationMatrix::rotate_x(alpha);
+  ASSERT_TRUE(approx_equal(Rx, RotationMatrix(EulerAngles::rotate_x(alpha))));
+  ASSERT_TRUE(approx_equal(Rx, RotationMatrix(AxisAngle::rotate_x(alpha))));
+  ASSERT_TRUE(approx_equal(Rx, RotationMatrix(Quaternion::rotate_x(alpha))));
+
+  auto Ry = RotationMatrix::rotate_y(beta);
+  ASSERT_TRUE(approx_equal(Ry, RotationMatrix(EulerAngles::rotate_y(beta))));
+  ASSERT_TRUE(approx_equal(Ry, RotationMatrix(AxisAngle::rotate_y(beta))));
+  ASSERT_TRUE(approx_equal(Ry, RotationMatrix(Quaternion::rotate_y(beta))));
+
+  auto Rz = RotationMatrix::rotate_z(gamma);
+  ASSERT_TRUE(approx_equal(Rz, RotationMatrix(EulerAngles::rotate_z(gamma))));
+  ASSERT_TRUE(approx_equal(Rz, RotationMatrix(AxisAngle::rotate_z(gamma))));
+  ASSERT_TRUE(approx_equal(Rz, RotationMatrix(Quaternion::rotate_z(gamma))));
+}
+
 TEST(Rotations, EulerAngles) {
-  auto R = RotationMatrix::rotate_x(0.5);
-  EXPECT_EQ(RotationMatrix::rotate_x(0.1).data, RotationMatrix::rotate_x(0.1).data);
+  double alpha = 0.12;
+  double beta = 0.23;
+  double gamma = 0.71;
+  auto Rx = EulerAngles::rotate_x(alpha);
+  ASSERT_TRUE(approx_equal(Rx, EulerAngles(RotationMatrix::rotate_x(alpha))));
+  // ASSERT_TRUE(approx_equal(Rx, EulerAngles(AxisAngle::rotate_x(alpha))));
+  ASSERT_TRUE(approx_equal(Rx, EulerAngles(Quaternion::rotate_x(alpha))));
+
+  auto Ry = EulerAngles::rotate_y(beta);
+  ASSERT_TRUE(approx_equal(Ry, EulerAngles(RotationMatrix::rotate_y(beta))));
+  // ASSERT_TRUE(approx_equal(Ry, EulerAngles(AxisAngle::rotate_y(beta))));
+  ASSERT_TRUE(approx_equal(Ry, EulerAngles(Quaternion::rotate_y(beta))));
+
+  auto Rz = EulerAngles::rotate_z(gamma);
+  ASSERT_TRUE(approx_equal(Rz, EulerAngles(RotationMatrix::rotate_z(gamma))));
+  // ASSERT_TRUE(approx_equal(Rz, EulerAngles(AxisAngle::rotate_z(gamma))));
+  ASSERT_TRUE(approx_equal(Rz, EulerAngles(Quaternion::rotate_z(gamma))));
+}
+
+TEST(Rotations, AxisAngle) {
+  double alpha = 0.12;
+  double beta = 0.23;
+  double gamma = 0.71;
+  auto Rx = AxisAngle::rotate_x(alpha);
+  ASSERT_TRUE(approx_equal(Rx, AxisAngle(RotationMatrix::rotate_x(alpha))));
+  // ASSERT_TRUE(approx_equal(Rx, AxisAngle(EulerAngles::rotate_x(alpha))));
+  ASSERT_TRUE(approx_equal(Rx, AxisAngle(Quaternion::rotate_x(alpha))));
+
+  auto Ry = AxisAngle::rotate_y(beta);
+  ASSERT_TRUE(approx_equal(Ry, AxisAngle(RotationMatrix::rotate_y(beta))));
+  // ASSERT_TRUE(approx_equal(Ry, AxisAngle(EulerAngles::rotate_y(beta))));
+  ASSERT_TRUE(approx_equal(Ry, AxisAngle(Quaternion::rotate_y(beta))));
+
+  auto Rz = AxisAngle::rotate_z(gamma);
+  ASSERT_TRUE(approx_equal(Rz, AxisAngle(RotationMatrix::rotate_z(gamma))));
+  // ASSERT_TRUE(approx_equal(Rz, AxisAngle(EulerAngles::rotate_z(gamma))));
+  ASSERT_TRUE(approx_equal(Rz, AxisAngle(Quaternion::rotate_z(gamma))));
 }
 
 int main(int argc, char **argv) {
