88d48b257a0f50dd7abfc2a32162ff3fdf19c347
Sebastian Kraemer <basti.kr@gmail.com>
2017-12-05 23:50:10 +0100

    Implement Point2D - Vector2D.

--- include/geometry/geometry.h
+++ include/geometry/geometry.h
@@ -17,6 +17,7 @@ struct Point2D {
   double y() const {return data[1];}
 
   Point2D operator+(const Vector2D& vector) const;
+  Point2D operator-(const Vector2D& vector) const;
   Vector2D operator-(const Point2D& point) const;
 
   std::array<double, 2> data;
--- src/point2d.cc
+++ src/point2d.cc
@@ -7,7 +7,11 @@ Point2D Point2D::operator+(const Vector2D& vector) const {
 }
 
 Vector2D Point2D::operator-(const Point2D& point) const {
-    return Vector2D(x()-point.x(), y()-point.y());
-  }
+  return Vector2D(x()-point.x(), y()-point.y());
+}
+
+Point2D Point2D::operator-(const Vector2D& vector) const {
+  return Point2D(x()-vector.x(), y()-vector.y());
+}
 
 } // namespace geometry
