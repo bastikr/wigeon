b05aa5e5cabdb64126f103b3f6bfa2bf12cf5b3b
Sebastian Kraemer <basti.kr@gmail.com>
2017-12-12 09:09:13 +0100

    Distance between line segments.

--- src/distances.cc
+++ src/distances.cc
@@ -29,50 +29,34 @@ double distance2(const LineSegment2D& segment0, const Point2D& point0) {
   return distance2(point0, segment0);
 }
 
-// double distance2(const LineSegment2D& segment0, const LineSegment2D& segment1) {
-//   double d = distance2(segment0.getPoint0(), segment1);
-//   d = min(distance2(segment0.getPoint1(), segment1));
-//   d = min(distance2(segment1.getPoint0(), segment0));
-//   d = min(distance2(segment1.getPoint1(), segment0));
-//   if segmentsintersect(segment0, segment1)
-//     return -d;
-//   return d;
-// }
+bool isintersecting(const LineSegment2D& segment0, const LineSegment2D& segment1) {
+  Vector2D v = segment0.getPoint1() - segment0.getPoint0();
+  Vector2D w1 = segment1.getPoint0() - segment0.getPoint0();
+  Vector2D w2 = segment1.getPoint1() - segment0.getPoint0();
+  double alpha = v*v;
 
-// bool isintersecting(const LineSegment2D& segment0, const LineSegment2D& segment1) {
-//   Vector2D v = segment0.getPoint1() - segment0.getPoint0();
-//   Vector2D w1 = segment1.getPoint0() - segment0.getPoint0();
-//   Vector2D w2 = segment1.getPoint1() - segment0.getPoint0();
-//   double alpha = v*v;
-//
-//   double c1 = cross(v, w1);
-//   double c2 = cross(v, w2);
-//   if (c1==0 && c2==0) // segment is on line
-//     double r1 = (v*w1)/alpha;
-//     double r2 = (v*w2)/alpha;
-//     return !((r1<0 && r2<0) || (r1>1 && r2>1));
-//   if (c1==0) // first segment point is on line
-//     r = (v*w1)/alpha;
-//     return 0 <= r && r <= 1;
-//   if (c2==0) // second segment point is on line
-//     r = (v*w2)/alpha;
-//     return 0 <= r && r <= 1;
-//   return c1*c2 < 0;
-// }
+  double c1 = cross(v, w1);
+  double c2 = cross(v, w2);
 
+  if ((c1>=0 && c2>=0) || (c1<=0 && c2<=0)) {
+    return false;
+  } else {
+    double r1 = (v*w1)/alpha;
+    double r2 = (v*w2)/alpha;
+    if (0<r1 && r1<1 && 0<r2 && r2<1)
+      return true;
+  }
+  return false;
+}
 
-// double distance2(const Point2D& point0, const Polygon2D& polygon0) {
-//   double d = distance2(point0, *polygon0.getLineSegment(0));
-//   for (int i=1; i<polygon0.size(); ++i) {
-//     d = std::min(d, distance2(point0, *polygon0.getLineSegment(i)));
-//   }
-//   return d;
-// }
-// double distance2(const Polygon2D& polygon0, const Point2D& point0) {
-//   return distance2(point0, polygon0);
-// }
-// double distance2(const LineSegment2D& segment, const Polygon2D& polygon) {}
-// double distance2(const Polygon2D& polygon, const LineSegment2D& segment) {}
-// distance(const Polygon2D& polygon0, const Polygon2D polygon1) {}
+double distance2(const LineSegment2D& segment0, const LineSegment2D& segment1) {
+  double d = distance2(segment0.getPoint0(), segment1);
+  d = std::min(d, distance2(segment0.getPoint1(), segment1));
+  d = std::min(d, distance2(segment1.getPoint0(), segment0));
+  d = std::min(d, distance2(segment1.getPoint1(), segment0));
+  if (isintersecting(segment0, segment1))
+    return -d;
+  return d;
+}
 
 } // namespace geometry
--- tests/test_distances.cpp
+++ tests/test_distances.cpp
@@ -36,3 +36,27 @@ TEST(DISTANCES, POINT_LINESEGMENT) {
   ASSERT_DOUBLE_EQ(d0, 2.3*2.3 + 1.7*1.7);
   ASSERT_DOUBLE_EQ(d1, 2.3*2.3 + 1.7*1.7);
 };
+
+TEST(DISTANCES, SEGMENT_SEGMENT) {
+  LineSegment2D segment0(Point2D(0, 0), Point2D(3, 0));
+  LineSegment2D segment1(Point2D(0, 1), Point2D(0, 3));
+  LineSegment2D segment2(Point2D(2, 0), Point2D(4, 0));
+  LineSegment2D segment3(Point2D(-1.8, 1.5), Point2D(1.8, 1.5));
+  double d = distance2(segment0, segment1);
+  ASSERT_DOUBLE_EQ(d, 1);
+
+  d = distance2(segment0, segment2);
+  ASSERT_DOUBLE_EQ(d, 0);
+
+  d = distance2(segment0, segment3);
+  ASSERT_DOUBLE_EQ(d, 1.5*1.5);
+
+  d = distance2(segment1, segment2);
+  ASSERT_DOUBLE_EQ(d, 5);
+
+  d = distance2(segment1, segment3);
+  ASSERT_DOUBLE_EQ(d, -0.5*0.5);
+
+  d = distance2(segment2, segment3);
+  ASSERT_DOUBLE_EQ(d, 1.5*1.5+0.2*0.2);
+};
