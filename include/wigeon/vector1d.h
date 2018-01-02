f063bc334865836c129a02cb33e0b2a37025ac64
Sebastian Kraemer <basti.kr@gmail.com>
2018-01-02 18:51:24 +0100

    Change order in vector1d.h

--- include/wigeon/vector1d.h
+++ include/wigeon/vector1d.h
@@ -17,12 +17,6 @@ struct Vector1D {
 };
 
 
-struct UnitVector1D : public Vector1D {
-  UnitVector1D(double x);
-  UnitVector1D(const Vector1D& v) : Vector1D(v.x()<0 ? -1 : 0) {}
-};
-
-
 Vector1D operator+(const Vector1D& vector0, const Vector1D& vector1);
 
 Vector1D operator-(const Vector1D& vector);
@@ -33,5 +27,11 @@ Vector1D operator*(double a, const Vector1D& vector);
 double operator*(const Vector1D& vector0, const Vector1D& vector1);
 
 Vector1D operator/(const Vector1D& vector, double a);
+
+
+struct UnitVector1D : public Vector1D {
+  UnitVector1D(double x);
+  UnitVector1D(const Vector1D& v) : Vector1D(v.x()<0 ? -1 : 0) {}
+};
 
 } // namespace wigeon
