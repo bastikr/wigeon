e35aea045c0fcddfc03a8a068ea9fda5d60814ad
Sebastian Kraemer <basti.kr@gmail.com>
2018-01-02 19:07:28 +0100

    Add test for 2d cross product.

--- tests/test_vector2d.cpp
+++ tests/test_vector2d.cpp
@@ -43,3 +43,12 @@ TEST(VECTOR2D, OPERATORS) {
   ASSERT_DOUBLE_EQ(v.x(), 0.5);
   ASSERT_DOUBLE_EQ(v.y(), 1);
 };
+
+TEST(VECTOR2D, CROSS) {
+  UnitVector2D u0(1, 1);
+  UnitVector2D u1(-1, 1);
+
+  ASSERT_DOUBLE_EQ(cross(u0,u1), 1);
+  ASSERT_DOUBLE_EQ(cross(u0,u0), 0);
+  ASSERT_DOUBLE_EQ(cross(u1,u1), 0);
+};
