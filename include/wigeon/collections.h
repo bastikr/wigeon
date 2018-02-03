5aab6012ce2ef0534cec2625f2671c7a8b84eff8
Sebastian Kraemer <basti.kr@gmail.com>
2018-02-03 08:45:55 +0100

    Implement vector addition for collections.

--- include/wigeon/collections.h
+++ include/wigeon/collections.h
@@ -2,6 +2,8 @@
 
 #include <list>
 
+#include "wigeon/rotations2d.h"
+#include "wigeon/vector2d.h"
 #include "wigeon/concepts.h"
 
 
@@ -10,5 +12,68 @@ namespace wigeon {
 using Curves2D = std::list<Curve2D>;
 using ClosedCurves2D = std::list<ClosedCurve2D>;
 using PlotObjects2D = std::list<PlotObject2D>;
+
+template <typename T>
+std::list<T> operator+(const Vector2D&, const std::list<T>&);
+
+template <typename T>
+std::list<T> operator+(const std::list<T>&, const Vector2D&);
+
+template <typename T>
+std::list<T> operator-(const Vector2D&, const std::list<T>&);
+
+template <typename T>
+std::list<T> operator-(const std::list<T>&, const Vector2D&);
+
+template <typename T>
+std::list<T> rotate(const Rotation2D&, const std::list<T>&);
+
+
+
+template <typename T>
+std::list<T> operator+(const Vector2D& vector, const std::list<T>& objects) {
+  std::list<T> result;
+  for (auto& object: objects) {
+    result.push_back(vector + object);
+  }
+  return result;
+}
+
+template <typename T>
+std::list<T> operator+(const std::list<T>& objects, const Vector2D& vector) {
+  std::list<T> result;
+  for (auto& object: objects) {
+    result.push_back(object + vector);
+  }
+  return result;
+}
+
+template <typename T>
+std::list<T> operator-(const Vector2D& vector, const std::list<T>& objects) {
+  std::list<T> result;
+  for (auto& object: objects) {
+    result.push_back(vector - object);
+  }
+  return result;
+}
+
+template <typename T>
+std::list<T> operator-(const std::list<T>& objects, const Vector2D& vector) {
+  std::list<T> result;
+  for (auto& object: objects) {
+    result.push_back(object - vector);
+  }
+  return result;
+}
+
+
+template <typename T>
+std::list<T> rotate(const Rotation2D& rotation, const std::list<T>& objects) {
+  std::list<T> result;
+  for (auto& object: objects) {
+    result.push_back(rotate(rotation, object));
+  }
+  return result;
+}
 
 } // namespace wigeon
---
+++ tests/test_collections.cpp
@@ -0,0 +1,40 @@
+#include "wigeon/collections.h"
+#include "gtest/gtest.h"
+
+#include <cmath>
+
+
+using namespace wigeon;
+
+TEST(COLLECTIONS, OPERATORS) {
+  ClosedCurves2D curves;
+  curves.push_back(Circle2D(Point2D(2, 4), 0.5));
+  ClosedCurves2D curves_p = curves + Vector2D(1, 2);
+  ClosedCurves2D curves_m = curves - Vector2D(1, 2);
+
+  ASSERT_DOUBLE_EQ(boost::get<Circle2D>(curves_p.front()).center().x(), 3);
+  ASSERT_DOUBLE_EQ(boost::get<Circle2D>(curves_p.front()).center().y(), 6);
+
+  ASSERT_DOUBLE_EQ(boost::get<Circle2D>(curves_m.front()).center().x(), 1);
+  ASSERT_DOUBLE_EQ(boost::get<Circle2D>(curves_m.front()).center().y(), 2);
+
+
+  curves_p = Vector2D(1, 2) + curves;
+  curves_m = Vector2D(1, 2) - curves;
+
+  ASSERT_DOUBLE_EQ(boost::get<Circle2D>(curves_p.front()).center().x(), 3);
+  ASSERT_DOUBLE_EQ(boost::get<Circle2D>(curves_p.front()).center().y(), 6);
+
+  ASSERT_DOUBLE_EQ(boost::get<Circle2D>(curves_m.front()).center().x(), -1);
+  ASSERT_DOUBLE_EQ(boost::get<Circle2D>(curves_m.front()).center().y(), -2);
+}
+
+TEST(COLLECTIONS, ROTATION) {
+  ClosedCurves2D curves;
+  curves.push_back(Circle2D(Point2D(2, 4), 0.5));
+  ClosedCurves2D curves_rotated = rotate(M_PI/2, curves);
+
+  ASSERT_DOUBLE_EQ(boost::get<Circle2D>(curves_rotated.front()).center().x(), -4);
+  ASSERT_DOUBLE_EQ(boost::get<Circle2D>(curves_rotated.front()).center().y(), 2);
+  ASSERT_DOUBLE_EQ(boost::get<Circle2D>(curves_rotated.front()).radius(), 0.5);
+}
